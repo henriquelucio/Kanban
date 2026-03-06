a1313934e82a2eacf49c0a5709ae51ed94087719
Henrique <henriquelucio95@hotmail.com>
2026-03-06 20:21:05 -0300

    delete and complete function

--- main.js
+++ main.js
@@ -9,9 +9,17 @@ addTaskBtnEl.addEventListener("click", addNewTask);
 //Functions
 function addNewTask(){
     if(newTaskInputEl.value.trim() !== ""){
-    const newTaskItem = document.createElement("li");
-    newTaskItem.textContent = newTaskInputEl.value;
-    unorderedListEl.appendChild(newTaskItem);
+    const newTaskLi = document.createElement("li");
+    newTaskLi.innerHTML = newTaskInputEl.value + ' <button class="done-btn">O</button> <button class="delete-btn">X</button>';
+    const deleteBtn = newTaskLi.querySelector(".delete-btn");
+    const doneBtn = newTaskLi.querySelector(".done-btn");
+    deleteBtn.addEventListener("click", () =>{
+        newTaskLi.remove();
+    });
+    doneBtn.addEventListener("click", () =>{
+        newTaskLi.classList.toggle("completed");
+    });
+    unorderedListEl.prepend(newTaskLi);
     newTaskInputEl.value = "";
     }else{
         alert("Type the task in input field!");
